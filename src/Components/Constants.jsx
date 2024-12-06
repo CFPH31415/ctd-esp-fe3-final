54f59a1efaac4eb4520eb2e218b6c70e8bdb35f2
Carlos Portillo <carlosportillo308@gmail.com>
2024-12-06 17:38:43 -0500

    guarda favs en local storage

--- src/Components/Constants.jsx
+++ src/Components/Constants.jsx
@@ -1,4 +1,7 @@
+// Recupera los favoritos guardados en localStorage
+const savedFavs = JSON.parse(localStorage.getItem('favs')) || [];
+
 export const initialState = {
   theme: 'light',
-  favs: [],
+  favs: savedFavs,
 };
